c71bf2cfc036d311397f543fdea79482e94111c9
Nexus <yaroslaw3718@gmail.com>
2025-06-18 21:49:36 +0300

    add sum function and documentation

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
-using std::cout, std::endl;
+using std::cout;
+
+int Sum(int a, int b);
+
 int main() {
     cout<<"Hello World!\n";
     cout<<"New info\n";
 
-    cout<<"Even newer info\n";
+    cout<<Sum(16,7)<<"\n";
 }
